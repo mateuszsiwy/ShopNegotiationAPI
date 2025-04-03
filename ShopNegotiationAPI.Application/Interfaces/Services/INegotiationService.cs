b43a3ab778311515a8fa9c3a93a70e90a53d9f23
msiwy12345 <mateuszsiwy00@gmail.com>
2025-04-03 21:50:20 +0200

    Add service interfaces for Negotiation and Product management

---
+++ ShopNegotiationAPI.Application/Interfaces/Services/INegotiationService.cs
@@ -0,0 +1,19 @@
+﻿using ShopNegotiationAPI.Domain.Models;
+
+namespace ShopNegotiationAPI.Application.Interfaces.Services;
+
+public interface INegotiationService
+{
+    Task<IEnumerable<Negotiation>> GetAllNegotiationsAsync();
+    Task<Negotiation> GetNegotiationByIdAsync(int id);
+    Task<IEnumerable<Negotiation>> GetNegotiationsByProductIdAsync(int productId);
+    Task<Negotiation> AddNegotiationAsync(Negotiation negotiation);
+    Task<Negotiation> UpdateNegotiationAsync(Negotiation negotiation);
+    Task<bool> DeleteNegotiationAsync(int id);
+    Task<bool> NegotiationExistsAsync(int id);
+    
+    Task<Negotiation> ProcessNegotiationAsync(int negotiationId, decimal proposedPrice);
+    Task<Negotiation> FinalizeNegotiationAsync(int negotiationId, bool isAccepted);
+    Task<IEnumerable<Negotiation>> GetActiveNegotiationsAsync();
+    Task<IEnumerable<Negotiation>> GetExpiredNegotiationsAsync();
+}
---
+++ ShopNegotiationAPI.Application/Interfaces/Services/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using ShopNegotiationAPI.Domain.Models;
+
+namespace ShopNegotiationAPI.Application.Interfaces.Services;
+
+public interface IProductService
+{
+    Task<IEnumerable<Product>> GetAllProductsAsync();
+    Task<Product> GetProductByIdAsync(int id);
+    Task<Product> AddProductAsync(Product product);
+    Task<Product> UpdateProductAsync(Product product);
+    Task<bool> DeleteProductAsync(int id);
+    Task<bool> ProductExistsAsync(int id);
+}
